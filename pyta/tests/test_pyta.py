424b76750b8711ee31ee432622363457adb9fc66
hcherkaoui <hamza.cherkaoui@polytechnique.edu>
2020-05-24 16:45:02 +0200

    TEST fix tests

--- pyta/tests/test_pyta.py
+++ pyta/tests/test_pyta.py
@@ -7,31 +7,34 @@ import pytest
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import approx_fprime
-
-from pyta import _prox_t
-from pyta.utils import (estimate_Lipsch_cst, spm_hrf, fista, _soft_th, _obj_t,
-                        _grad_t)
+from prox_tv import tv1_1d
+from pyta.loss_and_grad import _obj_t_analysis, _grad_t_analysis
+from pyta.optim import fista
+from pyta.hrf_model import double_gamma_hrf, make_toeplitz
+from pyta.utils import estimate_Lipsch_cst
 
 
 def test_gradient():
     """ Test the gradients.
     """
-    h = spm_hrf(1.0, 30)
+    h = double_gamma_hrf(1.0, 30)
     n_times = 100
     n_times_valid = n_times - len(h) + 1
 
-    y = np.random.randn(n_times)
-    x0 = np.random.randn(n_times_valid)
+    y = np.random.randn(1, n_times)
+    x0 = np.random.randn(1, n_times_valid)
 
-    hth = np.convolve(h[::-1], h)
-    hty = np.convolve(h[::-1], y, mode='valid')
+    H = make_toeplitz(h, n_times_valid).T
+    HtH = H.dot(H.T)
+    Hty = y.dot(H.T)
 
     def grad(x):
-        # global: y, hth, hty
-        return _grad_t(x, hth, hty)
+        return _grad_t_analysis(x, HtH, Hty)
 
     def approx_grad(x):
-        return approx_fprime(x, _obj_t, 1.0e-12, y, h, 0.0)
+        def obj(x):
+            return _obj_t_analysis(x, y, H, 0.0)
+        return approx_fprime(x.ravel(), obj, epsilon=1e-10)
 
     err = np.linalg.norm(approx_grad(x0) - grad(x0))
     err /= np.linalg.norm(approx_grad(x0))
@@ -39,8 +42,8 @@ def test_gradient():
         assert err < 1.0e-2
     except AssertionError as e:
         plt.figure("[{0}] Failed estimation".format("approx-grad/grad"))
-        plt.plot(approx_grad(x0), 'b--', lw=3.0, label="approx-grad")
-        plt.plot(grad(x0), 'y-', lw=3.0, label="grad")
+        plt.plot(approx_grad(x0).T, lw=3.0, label="approx-grad")
+        plt.plot(grad(x0).T, ls='--', lw=3.0, label="grad")
         plt.title("Approximated gradient / closed-form gradient")
         plt.legend()
         plt.show()
@@ -48,39 +51,37 @@ def test_gradient():
 
 
 @pytest.mark.parametrize('momentum', [True, False])
-@pytest.mark.parametrize('step_size', [None, 1.0e-6])
 @pytest.mark.parametrize('max_iter', [1, 10, 100])
-def test_fista_decrease(momentum, step_size, max_iter):
+def test_fista_decrease(momentum, max_iter):
     """ Test that the BOLD-TV cost function deacrease.
     """
-    h = spm_hrf(1.0, 30)
+    h = double_gamma_hrf(1.0, 30)
     n_times = 100
     n_times_valid = n_times - len(h) + 1
 
+    y = np.random.randn(1, n_times)
+    x0 = np.random.randn(1, n_times_valid)
+
+    H = make_toeplitz(h, n_times_valid).T
+    HtH = H.dot(H.T)
+    Hty = y.dot(H.T)
+
+    step_size = np.linalg.norm(HtH, ord=2) ** 2
     lbda = 1.0
 
-    y = np.random.randn(n_times)
-    x0 = np.random.randn(n_times_valid)
-
-    hth = np.convolve(h[::-1], h)
-    hty = np.convolve(h[::-1], y, mode='valid')
-
     def grad(x):
-        # global: y, hth, hty
-        return _grad_t(x, hth, hty)
+        return _grad_t_analysis(x, HtH, Hty)
 
     def obj(x):
-        # global: y, h, lbda
-        return _obj_t(x, y, h, lbda=0.0)
+        return _obj_t_analysis(x, y, H, lbda=lbda)
 
     def prox(x, step_size):
-        # global: lbda
-        return _soft_th(x, lbda, step_size)
+        return np.r_[[tv1_1d(x_, lbda * step_size) for x_ in x]]
 
     x1 = fista(grad, obj, prox, x0, momentum=momentum, max_iter=max_iter,
                step_size=step_size)
 
-    assert _obj_t(x0, y, h, lbda) >= _obj_t(x1, y, h, lbda)
+    assert _obj_t_analysis(x0, y, H, lbda) >= _obj_t_analysis(x1, y, H, lbda)
 
 
 def test_estimate_Lipsch_cst():
@@ -105,11 +106,9 @@ def test_fista_optimum(momentum, step_size):
     y = np.random.randn(n_times_valid)
 
     def grad(x):
-        # global: y
         return x - y
 
     def obj(x):
-        # global: y
         return 0.5 * np.sum(np.square(x - y))
 
     def prox(x, step_size):
@@ -118,8 +117,9 @@ def test_fista_optimum(momentum, step_size):
     if step_size == '1/Lipsch':
         def AtA(x):
             return x
+        step_size = 0.9 / estimate_Lipsch_cst(AtA, n_times_valid,
+                                              max_iter=1000)
 
-        step_size = 0.9 / estimate_Lipsch_cst(AtA, n_times_valid)
     x = fista(grad, obj, prox, np.zeros_like(y), momentum=momentum,
               max_iter=100, step_size=step_size)
 
@@ -129,45 +129,6 @@ def test_fista_optimum(momentum, step_size):
         plt.figure("[{0}] Failed estimation".format(__file__))
         plt.plot(y, 'b--', lw=3.0, label="Ref")
         plt.plot(x, 'y-', lw=3.0, label="Test")
-        plt.title("Expected vs estimated signal")
-        plt.legend()
-        plt.show()
-        raise e
-
-
-@pytest.mark.parametrize('momentum', [True, False])
-@pytest.mark.parametrize('step_size', [None, '1/Lipsch', 1.0e-6])
-def test_prefect_recover(momentum, step_size):
-    """ With True HRF, no regularization, prox_t call and an init at the
-    True value.
-    """
-    h = spm_hrf(1.0, 30)
-    n_times = 100
-    n_times_valid = n_times - len(h) + 1
-
-    lbda = 0.0
-
-    x = np.random.randn(n_times_valid)
-    y = np.convolve(h, np.cumsum(x))
-
-    hth = np.convolve(h[::-1], h)
-
-    if step_size == '1/Lipsch':
-        def AtA(x):
-            return _grad_t(x, hth, hty=None)
-        step_size = 0.9 / estimate_Lipsch_cst(AtA, n_times_valid)
-
-    solver_kwargs = dict(max_iter=100, step_size=step_size,
-                         momentum=momentum, x0=x)
-    x_ = _prox_t(y, lbda, h, solver_kwargs=solver_kwargs)
-
-    try:
-        np.testing.assert_allclose(x, x_)
-    except AssertionError as e:
-        plt.figure("[{0}][{1}, {2}] Failed estimation".format(
-                                          __file__, step_size, momentum))
-        plt.plot(x, 'b--', lw=3.0, label="Ref")
-        plt.plot(x_, 'y-', lw=3.0, label="Test")
         plt.title("Expected vs estimated signal")
         plt.legend()
         plt.show()
